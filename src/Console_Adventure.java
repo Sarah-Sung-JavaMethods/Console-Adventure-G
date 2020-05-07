4f731257ec8d4a4f28131745787fff76046c5f5e
SUNG IL LEE <sungil.lee90@gmail.com>
2020-05-06 22:38:36 -0500

    Tried to correct the infinite loop for askUser.

--- src/Console_Adventure.java
+++ src/Console_Adventure.java
@@ -9,32 +9,42 @@ public class Console_Adventure {
     // Ask if the user is ready to start. If they type in "yes", start the game.
     public static void keepAskingUser() {
         Scanner sc1 = new Scanner(System.in);
-
-        while(true) {
+        do {
             System.out.println("Do you want to start the game? [y/n]");
             String userInput = sc1.nextLine();
-
-            if (userInput.toLowerCase().equals("y")) {
-                askUserName();
-            } else {
+            if (!userInput.toLowerCase().equals("y")) {
                 System.out.println("Okay, goodbye!");
                 break;
+            } else {
+                System.out.println("What is your name?");
+                String userName = sc1.nextLine();
+                System.out.println("username: " + userName);
+                System.out.println("Hello, " + userName + "!");
             }
-
-        }
-
+        } while (false);
+//        while(true) {
+//            System.out.println("Do you want to start the game? [y/n]");
+//            String userInput = sc1.nextLine();
+//
+//            if (!userInput.toLowerCase().equals("y")) {
+//                System.out.println("Okay, goodbye!");
+//                break;
+//            } else {
+//                askUserName(sc1);
+//            }
+//        }
     }
 
     // Ask the user for their name. Store this as a variable to personalize the adventure.
-    public static void askUserName() {
-        Scanner sc2 = new Scanner(System.in);
+//    public static void askUserName(Scanner scan) {
+//
+//        do {
+//            System.out.println("What is your name?");
+//            String userName = scan.nextLine();
+//            System.out.println("username: " + userName);
+//            System.out.println("Hello, " + userName + "!");
+//        } while
+//    }
 
-        System.out.println("What is your name?");
-        String userName = sc2.nextLine();
-        System.out.println("username: " + userName);
-        System.out.println("Hello, " + userName + "!");
-
-
-    }
 
 }
